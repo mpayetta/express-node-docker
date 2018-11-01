4b65c9d0cfaebb0929749728e704ae00bdbb605a
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2018-11-01 10:23:53 +1000

    add eslint and apply rules to files

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
-var express = require('express');
-var router = express.Router();
-var mongodb = require('mongodb');
-var client = mongodb.MongoClient;
+var express = require('express')
+var router = express.Router()
+var mongodb = require('mongodb')
+var client = mongodb.MongoClient
 
-var uri = "mongodb://mongo/dummy-app";
+var uri = 'mongodb://mongo/dummy-app'
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' })
+})
 
 router.get('/data/from/db', function(req, res, next) {
-	client.connect(uri, function (err, db) {
-	    if (err) return next(err);
+  client.connect(uri, function (err, db) {
+    if (err) return next(err)
 
-    	var collection = db.collection('dummy');
-    	collection.find({}).toArray(function(err, docs) {
-			if (err) return next(err);
-			return res.json(docs);
-    	});
-	});
-});
+    var collection = db.collection('dummy')
+    collection.find({}).toArray(function(err, docs) {
+      if (err) return next(err)
+      return res.json(docs)
+    })
+  })
+})
 
 router.post('/data/into/db', function(req, res, next) {
-	client.connect(uri, function (err, db) {
-	    if (err) return next(err);
+  client.connect(uri, function (err, db) {
+    if (err) return next(err)
 
-    	var collection = db.collection('dummy');
-    	collection.insertMany(req.body, function(err, result) {
-			return res.json({ result: "success" });
-    	});
-	});
-});
+    var collection = db.collection('dummy')
+    collection.insertMany(req.body, function(err, result) {
+      return res.json({ result })
+    })
+  })
+})
 
-module.exports = router;
+module.exports = router
